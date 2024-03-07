be629bb5f8fd7404806105829a27d753c29d24a1
jefim <jefim.naljotov@stonybrook.edu>
2024-03-07 00:19:42 -0500

    second save

--- homework_two/problem_6.py
+++ homework_two/problem_6.py
@@ -1,19 +1,26 @@
 import time
 import numpy as np
 from numpy import linalg
+import matplotlib.pyplot as plt
 
-t = []
+times = []
+T = []
 
 
 def temperature_engine(time_s, initial_temperature_C):
     return initial_temperature_C * np.exp(-.1 * time_s)
 
 
-def temperature_simulation(starting_temp,  end_temp, step_size):
-    current_temp = temperature_engine()
+def temperature_simulation(starting_temp, end_temp, step_size):
+    current_temp = starting_temp
+    t0 = 0
+
     while current_temp >= end_temp:
-        current_temp -= step_size
-        t.append(current_temp)
+        t0 = t0 + step_size
+        times.append(t0)
+        current_temp = temperature_engine(t0, starting_temp)
+        T.append(current_temp)
 
 
-temperature_simulation(500, 500, 50)
+temperature_simulation(500, 50, step_size=.1)
+print(f'It takes {times[-1]} seconds to reach a temperature of {T[-1]} degrees Celsius')
